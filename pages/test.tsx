d87c1d1cf8437f576efb573c7c7cebfa962be16f
Grégoire Van Thienen <gregoire.vanthienen@outlook.com>
2023-04-25 00:22:26 +0200

    testing api

--- pages/test.tsx
+++ pages/test.tsx
@@ -5,11 +5,9 @@ import ScrollDialog from "@/components/scrolldialog";
 import { Typography } from "@mui/material";
 import PersistentDrawer from "@/components/persistentdrawer";
 import PostForm from "@/components/postform";
+import { fetchData } from "@/utils";
+import MyComponent from "@/components/testapi";
 
 export default function showTest() {
-    return (
-    <>
-    <PostForm/>
-    </>
-    )
+    return <MyComponent/>
 }
