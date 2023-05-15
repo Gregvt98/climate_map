797afd13705607c135c662e2d8e99c26b74af5dd
Grégoire Van Thienen <gregoire.vanthienen@outlook.com>
2023-05-15 12:24:03 +0200

    added expand to newsfeed

--- components/newsfeed.tsx
+++ components/newsfeed.tsx
@@ -12,43 +12,66 @@ import {
   DialogActions,
   Button,
 } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import { styled } from '@mui/material/styles';
+import Collapse from '@mui/material/Collapse';
 
 import NEWS from "../data/news.json";
 
 function NewsCard({ newsItem }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        {newsItem.urlToImage ? (
-          <Link href={newsItem.url} target="_new">
+    <Link href={newsItem.url} target="_new" underline="none">
+      <Card sx={{ maxWidth: 400 }} className="mt-3">
+        <CardActionArea>
+          {newsItem.urlToImage ? (
             <CardMedia
               component="img"
               height="140"
               image={newsItem.urlToImage}
-              alt="green iguana"
+              alt={newsItem.description}
             />
-          </Link>
-        ) : null}
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {newsItem.title}
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" component="div">
-            {newsItem.author}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {newsItem.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+          ) : null}
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              {newsItem.title}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="subtitle1"
+              component="div"
+              className="underline"
+            >
+              {newsItem.author}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {newsItem.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
 
 export default function NewsFeed() {
+  const [open, setOpen] = useState(true);
   const [news, setNews] = useState([]);
+  const [expanded, setExpanded] = React.useState(false);
   //add geolocation to show country specific news?
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   /**
    useEffect(() => {
@@ -74,25 +97,43 @@ export default function NewsFeed() {
     fetchNews();
   }, []);
    */
+  interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+  }
+
+  const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
 
   //change to dynamic with use effect
   console.log(NEWS[0]);
   return (
-    <div>
-      <Dialog
-        open={true}
-        scroll="paper"
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-      >
-        <DialogTitle id="scroll-dialog-title">News Feed</DialogTitle>
-        <DialogContent>
-          {NEWS.map((newsItem, index) => (
-            <NewsCard key={index} newsItem={newsItem}></NewsCard>
-          ))}
-        </DialogContent>
-        <DialogActions></DialogActions>
-      </Dialog>
+    <div className="absolute bottom-10 right-12 w-[310px] max-w-[310px] max-h-[580px] bg-white shadow-md p-4 mt-2 text-sm text-gray-600 outline-none overflow-auto">
+      <div className="flex items-center flex-initial bg-gray-800 text-white py-4 pl-6 mt-2">
+        <h5>News Feed</h5>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon className="text-white"/>
+        </ExpandMore>
+      </div>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <div className="bg-grey">
+        {NEWS.map((newsItem, index) => (
+          <NewsCard key={index} newsItem={newsItem}></NewsCard>
+        ))}
+      </div>
+      </Collapse>
     </div>
   );
 }
